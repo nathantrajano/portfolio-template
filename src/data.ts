8ce531a1f4ef0c59ba7b657bf12ff3d31df04d15
nathantrajano <nathan.amaro.trajano@ccc.ufcg.edu.br>
2025-11-30 17:06:52 -0300

    update

--- src/data.ts
+++ src/data.ts
@@ -1,16 +1,16 @@
 import { AboutMeData, Game, GameEngine, LinkImageSource, MediaType, Platform } from "./types";
 
 export const PersonalInfo: AboutMeData = {
-  name: "Nathan Amaro Trajano",
+  name: "Nathan Trajano",
   role: "Game Developer",
-  introduction: "Hi, I'm Nathan, a passionate game developer.",
+  introduction: "Hi, I'm Nathan, a passionate game developer and software engineer.",
   description: "I love creating immersive experiences through games.",
-  image: `${process.env.PUBLIC_URL}/images/Profile.jpg`,
-  cvUri: `${process.env.PUBLIC_URL}/files/YOUR_CV.pdf`,
+  image: `${process.env.PUBLIC_URL}/images/IMG-20250401-WA0009.jpg`,
+  cvUri: `${process.env.PUBLIC_URL}/files/Curriculo GameDev Nathan Amaro Trajano.pdf`,
   links: {
     github: "https://github.com/nathantrajano",
     itchIO: "https://NAME.itch.io",
-    linkedIn: "https://www.linkedin.com/in/NAME",
+    linkedIn: "https://www.linkedin.com/in/nathan-trajano-47264a288/",
   }
 };
 
@@ -29,12 +29,13 @@ export const games: Game[] = [
       { source: "/images/games/GAME_NAME/image.png", type: MediaType.Image },
       { source: "/images/games/GAME_NAME/imageEditor.png", type: MediaType.Image },
       { source: "/images/games/GAME_NAME/MainMenu.png", type: MediaType.Image },
+      { source: "https://www.youtube.com/embed/P03wxXw1lo4", type: MediaType.YouTube },
     ],
   },
   {
-    name: "GAME_NAME",
-    description: "GAME_DESCRIPTION",
-    genres: ["GAME_GENRE", "GAME_GENRE..."],
+    name: "MaquinoMaquia Arena",
+    description: "O projeto MaquinoMaquia Arena é um fighting game que promete misturar elementos de fantasia e sci-fi em um universo rico e dinâmico. Atualmente em fase de prototipação, o jogo está sendo desenvolvido visando a sua apresentação no evento GameDev Quest.",
+    genres: ["Luta", "Sci-Fi"],
     platforms: [Platform.Windows],
     engine: GameEngine.Unity,
     links: [
@@ -42,11 +43,9 @@ export const games: Game[] = [
       { source: LinkImageSource.ItchIo, url: "https://YOUR_ITCH.itch.io/GAME_NAME" },
     ],
     media: [
-      { source: "/images/games/GAME_NAME/Screenshot_1.png", type: MediaType.Image },
-      { source: "https://www.youtube.com/embed/dQw4w9WgXcQ", type: MediaType.YouTube },
-      { source: "/images/games/GAME_NAME/Screenshot_2.png", type: MediaType.Image },
-      { source: "/images/games/GAME_NAME/Screenshot_3.png", type: MediaType.Image },
-      { source: "/images/games/GAME_NAME/Screenshot_4.png", type: MediaType.Image },
+      { source: "/images/games/GAME_NAME/MaquinoMaquia/MaquinoMaquiaIdle.png", type: MediaType.Image },
+      { source: "/images/games/GAME_NAME/MaquinoMaquia/Chute.png", type: MediaType.Image },
+      { source: "/images/games/GAME_NAME/MaquinoMaquia/Soco.png", type: MediaType.Image },
     ],
   }
 ];
