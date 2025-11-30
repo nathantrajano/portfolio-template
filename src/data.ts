192b41f520a52477ff488e9eca2ba277495ff06b
nathantrajano <nathan.amaro.trajano@ccc.ufcg.edu.br>
2025-11-30 12:12:34 -0300

    update

--- src/data.ts
+++ src/data.ts
@@ -18,19 +18,17 @@ export const games: Game[] = [
   {
     name: "Apulgalipse",
     description: "Apulgalipse é um jogo desenvolvido como projeto da disciplina de Engenharia de Software da Universidade Federal de Campina Grande (UFCG), no período 2025.1.\nNeste jogo, o prédio do CAA enfrenta o pior pesadelo de sua história: uma infestação apocalíptica de pulgas. Prepare-se para encarar coceiras épicas, bosses pulguentos e salvar os alunos antes que seja tarde demais.",
-    genres: ["GAME_GENRE", "GAME_GENRE..."],
+    genres: ["Aventura", "Sátira"],
     platforms: [Platform.Windows],
     engine: GameEngine.Unity,
     links: [
-      { source: LinkImageSource.Github, url: "https://github.com/YOUR_REPO" },
+      { source: LinkImageSource.Github, url: "https://github.com/grupo5-ES-2025-1/Apulgalipse" },
       { source: LinkImageSource.ItchIo, url: "https://YOUR_ITCH.itch.io/GAME_NAME" },
     ],
     media: [
-      { source: "/images/games/GAME_NAME/Screenshot_1.png", type: MediaType.Image },
-      { source: "https://www.youtube.com/embed/dQw4w9WgXcQ", type: MediaType.YouTube },
-      { source: "/images/games/GAME_NAME/Screenshot_2.png", type: MediaType.Image },
-      { source: "/images/games/GAME_NAME/Screenshot_3.png", type: MediaType.Image },
-      { source: "/images/games/GAME_NAME/Screenshot_4.png", type: MediaType.Image },
+      { source: "/images/games/GAME_NAME/image.png", type: MediaType.Image },
+      { source: "/images/games/GAME_NAME/imageEditor.png", type: MediaType.Image },
+      { source: "/images/games/GAME_NAME/MainMenu.png", type: MediaType.Image },
     ],
   },
   {
